2b37c179bb56f13802a6e96a93a43b4d54eb0627
Shone Binu <shonebinualias@gmail.com>
2023-12-08 22:38:54 +0530

    day 2 part 2 complete

--- 2023/day2/index.js
+++ 2023/day2/index.js
@@ -37,4 +37,58 @@ function part1(file, limRed, limGreen, limBlue) {
     console.log(sumId)
 }
 
-part1('./input.txt', 12, 13, 14);
+
+
+function part2(file) {
+    function updateMaxValues(input) {
+        Object.keys(input).forEach((color) => {
+            temp[color] = Math.max(temp[color] || 0, parseInt(input[color]))
+        })
+    }
+
+    const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
+
+    const games = [];
+    lines.forEach(line => {
+        games.push(line.split(': ')[1].split('; '))
+    })
+    
+    let turns = {};
+
+    games.forEach((game, i) => {
+        game.forEach((set, j) => {
+            set.split(', ').forEach(col => {
+                let [num, color] = col.split(' ');
+                turns[color] = num;
+            })
+            games[i][j] = turns;
+            turns = {  };
+       })
+    })
+
+    const maxInGame = [];
+    let temp = {red: 0, green: 0, blue: 0};    
+
+    games.forEach(game => {
+        game.forEach(set => {
+            updateMaxValues(set);
+        })
+        maxInGame.push(temp);
+        temp = {red: 0, green: 0, blue: 0}
+    })
+    
+    let sum = 0;
+
+    maxInGame.forEach(item => {
+        sum += (item.red * item.green * item.blue)
+    })
+    
+    console.log(sum)
+}
+
+
+
+
+
+//part1('./input.txt', 12, 13, 14);
+part2('./input.txt')
