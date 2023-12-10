6ca54ec31cc5a63885c9fc956adb5d46cdd951c9
Shone Binu <shonebinualias@gmail.com>
2023-12-10 14:45:35 +0530

    day 2

--- 2023/day3/index.js
+++ 2023/day3/index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 function part1(file) {
     const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
 
+    const pattern = /[^0-9.]/g;
+    const symbols = [];
     const regex = /\d+/g; 
     const matches = [];
     let match;
@@ -17,10 +19,21 @@ function part1(file) {
             })
         }
     })
+
+    lines.forEach((line, index) => {
+        while ((match = pattern.exec(line)) !== null) {
+            symbols.push({
+                sym: match[0], 
+                x: index,
+                y: match.index,
+                yend: match.index + match[0].length - 1
+            })
+        }
+    })
     
     console.log(matches)
-    
+    console.log(symbols)
 }
 
 
-part1('example.txt');
+part1('./example.txt');
